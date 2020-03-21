80ebf086a6c1b57c1bd80430cc580d58933367be
qngapparat <jakob.wallnoefer@outlook.com>
2020-03-21 13:41:39 +0100

    js: write fetched guardian data to CSV

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const { grd } = require('./connectors/guardian')
+const csvParser = require('json2csv').Parser 
+const fs = require('fs')
 
 async function main() {
   // get articles
@@ -13,16 +15,24 @@ async function main() {
     return {
       // TODO map month to SINE
       title: r.webTitle,
-      year: d.getFullYear(),
-      month: d.getMonth() + 1,
-      day: d.getDate(),
-      millis: d.getTime(), // use this temporarily
+      // year: d.getFullYear(),
+      // month: d.getMonth() + 1,
+      // day: d.getDate(),
+      // millis: d.getTime(), // use this temporarily
       // TODO daytime??
       section: r.sectionName.toLowerCase()
     }
   })
 
-  console.log(rs)
+  const csvfields = ['title', 'section']
+  const csvvalues = rs 
+
+  const json2csvParser = new csvParser({ fields: csvfields });
+  const csv = json2csvParser.parse(csvvalues)
+
+  console.log(csv)
+  fs.writeFileSync('./python/demoin.csv', csv)
+  console.log("Wrote csv to python/demoin.csv")
 }
 
 main()
